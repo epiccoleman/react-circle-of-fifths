828c660c05965ba31924d8acfdd4e214d92208a8
Eric Coleman <eric@epiccoleman.com>
2023-03-26 12:19:38 -0400

    ok... why doesn't work

--- src/CircleOfFifths.tsx
+++ src/CircleOfFifths.tsx
@@ -3,40 +3,60 @@
 import React from "react";
 
 function polarToCartesian(x, y, r, degrees) {
+    /**
+     * https://observablehq.com/@haakenlid/svg-circle
+     * Given a polar coordinate (r, degrees), converts to a cartesian coordinate, centered on (x,y)
+     *
+     * @param x - x offset for the center of the circle
+     * @param y - y offset for the center of the circle
+     * @param r - radius of the polar coordinate
+     * @param degrees - degrees of the polar coordinate
+     */
   const radians = (degrees * Math.PI) / 180.0;
   return [x + r * Math.cos(radians), y + r * Math.sin(radians)];
 }
 
 function segmentPath(x, y, r0, r1, d0, d1) {
+    /**
+     * https://observablehq.com/@haakenlid/svg-circle
+     * Generates an svg path for a segment of a circle
+     *
+     * @param x - x offset for the center of the circle
+     * @param y - y offset for the center of the circle
+     * @param r0 - outer radius of the segment, i.e. how far out it extends from the center of the circle
+     * @param r1 - inner radius of the segment - i.e. where it starts. if this were 0, the segment would touch the center
+     * @param d0 - where the segment starts in the circle - i.e. the degrees from reference of one edge
+     * @param d1 - where the segment end in the circle - i.e. the degrees from reference of other edge
+     */
   const arc = Math.abs(d0 - d1) > 180 ? 1 : 0;
   const point = (radius, degree) =>
     polarToCartesian(x, y, radius, degree)
       .map((n) => n.toPrecision(5))
       .join(",");
   return [
-    `M${point(r0, d0)}`, // Move to the
-    `A${r0},${r0},0,${arc},1,${point(r0, d1)}`,
-    `L${point(r1, d1)}`,
-    `A${r1},${r1},0,${arc},0,${point(r1, d0)}`,
-    "Z",
+    `M${point(r0, d0)}`, // Move to the starting point of the segment - one "corner"
+    `A${r0},${r0},0,${arc},1,${point(r0, d1)}`, // draw outer arc from that corner to the next corner
+    `L${point(r1, d1)}`, // draw the line to the inner radius
+    `A${r1},${r1},0,${arc},0,${point(r1, d0)}`, // draw the inner arc
+    "Z", // close the path - draw the line back to the start point.
   ].join("");
 }
 
-function segment(index, segments, size, radius, width) {
-    const center = size / 2
-    const degrees = 360 / segments
-    const start = degrees * index
-    const end = (degrees * (index + 1) + 1)
-    const path = segmentPath(center, center, radius, radius-width, start, end)
-    return `<path stroke="black" d="${path}" />`
-  }
+// function segment(index, segments, size, radius, width) {
+//     const center = size / 2
+//     const degrees = 360 / segments
+//     const start = degrees * index
+//     const end = (degrees * (index + 1) + 1)
+//     const path = segmentPath(center, center, radius, radius-width, start, end)
+//     return `<path stroke="black" stroke-width="2" d="${path}" />`
+//   }
 
-  function posXY(center, radius, angle) {
-    return [
-      center + radius * Math.cos(angle * Math.PI / 180.0),
-      center + radius * Math.sin(angle * Math.PI / 180.0)
-    ]
-  }
+//   function posXY(center, radius, angle) {
+//     return [
+//       center + radius * Math.cos(angle * Math.PI / 180.0),
+//       center + radius * Math.sin(angle * Math.PI / 180.0)
+//     ]
+//   }
 
 // todo: s is sharp, b is flat - figure out how to render em better?
 const CIRCLE_OF_FIFTHS_DATA = [
@@ -119,8 +139,9 @@ return <>
 
         <circle cx="50%" cy="50%" r="40%" stroke="currentColor" strokeWidth="5" fill="transparent" />
         {/* <path */}
+        {`<path stroke="black" stroke-width="2" d="${segmentPath(200, 200, 100, 10, 0, 45)}`}
 
-        {data.map((n) => segment(n, 12, 10, 10, 100 ))}
+        {/* {data.map((n) => segment(n, 12, 10, 10, 100 ))} */}
         {/* <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text> */}
 </svg>
 
