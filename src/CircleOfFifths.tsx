9d96864f1d6c2e76f59c3e9b41428481e1baf212
Eric Coleman <eric@epiccoleman.com>
2023-04-01 11:13:44 -0400

    click handling

--- src/CircleOfFifths.tsx
+++ src/CircleOfFifths.tsx
@@ -120,7 +120,9 @@ const CIRCLE_OF_FIFTHS_DATA = [
 
 export const CircleOfFifths = () => {
 
-const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+const handleClick = (keySelection) => {
+    console.log(`you clicked: ${keySelection}`)
+}
 
 return <>
     <div className="container py-4 m-auto">
@@ -129,19 +131,19 @@ return <>
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 400 400" >
 
-        <g style={{transform: "rotate(15deg)", transformOrigin: "200px 200px"}}>
+        <g style={{transform: "rotate(-15deg)", transformOrigin: "200px 200px"}}>
         {
-            ...data.map((v, i) => {
-                return <CircleOfFifthsWedge x={200} y={200} r0={180} r1={120} d0={i * 30} d1={((i+1) * 30)}/>
+            ...CIRCLE_OF_FIFTHS_DATA.map((v, i) => {
+                return <CircleOfFifthsWedge onClick={() => {handleClick(v.note)}} x={200} y={200} r0={180} r1={120} d0={i * 30} d1={((i+1) * 30)}/>
             })
         }
         {
-            ...data.map((v, i) => {
-                return <CircleOfFifthsWedge x={200} y={200} r0={120} r1={80} d0={i * 30} d1={((i+1) * 30)}/>
+            ...CIRCLE_OF_FIFTHS_DATA.map((v, i) => {
+                return <CircleOfFifthsWedge onClick={() => {handleClick(v.relativeMinor)}} x={200} y={200} r0={120} r1={80} d0={i * 30} d1={((i+1) * 30)}/>
             })
         }
         {
-            ...data.map((v, i) => {
+            ...CIRCLE_OF_FIFTHS_DATA.map((v, i) => {
                 return <CircleOfFifthsWedge x={200} y={200} r0={80} r1={50} d0={i * 30} d1={((i+1) * 30)}  />
             })
         }
@@ -150,19 +152,19 @@ return <>
         {
             ...CIRCLE_OF_FIFTHS_DATA.map((v, i) => {
                 const [center_x, center_y] = polarToCartesian(200, 200, 150, (i *30))
-                return <text style={{textAnchor: "middle", dominantBaseline: "central"}} x={center_x} y={center_y} >{v.note}</text>
+                return <text style={{textAnchor: "middle", dominantBaseline: "central", pointerEvents: "none"}} x={center_x} y={center_y} >{v.note}</text>
             })
         }
          {
             ...CIRCLE_OF_FIFTHS_DATA.map((v, i) => {
                 const [center_x, center_y] = polarToCartesian(200, 200, 100, (i *30))
-                return <text style={{textAnchor: "middle", dominantBaseline: "central", fontSize: "small"}} x={center_x} y={center_y} >{v.relativeMinor}</text>
+                return <text style={{textAnchor: "middle", dominantBaseline: "central", fontSize: "small", pointerEvents: "none"}} x={center_x} y={center_y} >{v.relativeMinor}</text>
             })
         }
         {
             ...CIRCLE_OF_FIFTHS_DATA.map((v, i) => {
                 const [center_x, center_y] = polarToCartesian(200, 200, 65, (i *30))
-                return <text style={{textAnchor: "middle", dominantBaseline: "central", fontSize: "xx-small"}} x={center_x} y={center_y} >{v.diminished}</text>
+                return <text style={{textAnchor: "middle", dominantBaseline: "central", fontSize: "xx-small", pointerEvents: "none"}} x={center_x} y={center_y} >{v.diminished}</text>
             })
         }
         </g>
@@ -172,7 +174,7 @@ return <>
 
 }
 
-const CircleOfFifthsWedge = ({x, y, r0, r1, d0, d1, } ) => {
+const CircleOfFifthsWedge = ({x, y, r0, r1, d0, d1, onClick}) => {
     /**
      * @param x - x offset for the center of the circle
      * @param y - y offset for the center of the circle
@@ -180,8 +182,9 @@ const CircleOfFifthsWedge = ({x, y, r0, r1, d0, d1, } ) => {
      * @param r1 - inner radius of the segment - i.e. where it starts. if this were 0, the segment would touch the center
      * @param d0 - where the segment starts in the circle - i.e. the degrees from reference of one edge
      * @param d1 - where the segment end in the circle - i.e. the degrees from reference of other edge
+     * @param onClick - onClick
      */
     return (
-        <path className="cf-wedge" d={segmentPath(x, y, r0, r1, d0, d1)} stroke="black" strokeWidth="2" fill="#84CC16" ></path>
+        <path onClick={onClick? onClick : null} className="cf-wedge" d={segmentPath(x, y, r0, r1, d0, d1)} stroke="black" strokeWidth="2" fill="#84CC16" ></path>
     )
 }
