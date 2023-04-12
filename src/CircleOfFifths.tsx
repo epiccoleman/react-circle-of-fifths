de965471939b54fdc77e45fe299eb21b5b56b4b2
Eric Coleman <eric@epiccoleman.com>
2023-04-12 19:38:03 -0400

    some refactors, add demo app

--- src/CircleOfFifths.tsx
+++ src/CircleOfFifths.tsx
@@ -1,125 +1,109 @@
 // Circle of Fifths component heavily inspired by: https://blog.logrocket.com/interactive-svg-circle-of-fifths/
 
 import * as React from "react";
+
+import { polarToCartesian, segmentPath } from "./CFMathUtils";
+import CIRCLE_OF_FIFTHS_DATA from "./CircleOfFifthsData";
+
 import "./CircleOfFifths.css"
 
-import CIRCLE_OF_FIFTHS_DATA from "./CircleOfFifthsData";
-import { polarToCartesian, segmentPath } from "./CFMathUtils";
-
-type CircleOfFifthsTheme = {
-    majorKeysFillColor?: string;
-    majorKeysFillColorHover?: string;
-    majorKeysFillColorSelected?: string;
-    minorKeysFillColor?: string;
-    minorKeysFillColorHover?: string;
-    minorKeysFillColorSelected?: string;
-    diminishedKeysFillColor?: string;
-
-    strokeColor?: string;
-    strokeWidth?: string;
-    textColor?: string;
+export type CircleOfFifthsProps = {
+    /**
+     * Selection callback. Receives the currently selected key as a string.
+     * @param selection
+     */
+    handleKeySelection: (selection: string) => void;
 }
-
-const defaultTheme = {
-    majorKeysFillColor: "#84CC16",
-    majorKeysFillColorHover: "#65A30D",
-    majorKeysFillColorSelected:  "#65A30D",
-    minorKeysFillColor: "#84CC16",
-    minorKeysFillColorHover: "#65A30D",
-    minorKeysFillColorSelected:  "#65A30D",
-    diminishedKeysFillColor: "#84CC16",
-
-    strokeColor: "black",
-    strokeWidth: "2px",
-    textColor: "black"
-} as CircleOfFifthsTheme;
-
-type CircleOfFifthsProps = {
-    theme?: CircleOfFifthsTheme;
-    handleKeySelection?: (selection: string) => void
-}
-
-const CF_SIZE = 400;
 
 export const CircleOfFifths = (props: CircleOfFifthsProps) => {
 
-    const handleClick = (keySelection) => {
-        if (props.handleKeySelection) {
-            props.handleKeySelection(keySelection);
-        }
+const handleClick = (keySelection) => {
+    if (props.handleKeySelection) {
+        props.handleKeySelection(keySelection);
     }
+    // console.log(`you clicked: ${keySelection}`)
+}
 
-    return <svg version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox={`0 0 ${CF_SIZE} ${CF_SIZE}`} >
+const CF_SIZE = 400;
+const CF_CENTER = CF_SIZE/2;
 
-            <g style={{transform: "rotate(-15deg)", transformOrigin: `${CF_SIZE/2}px ${CF_SIZE/2}px`}}>
-            {
-                ...CIRCLE_OF_FIFTHS_DATA.map((v, i) => {
-                    return <CircleOfFifthsWedge onClick={() => {handleClick(v.note)}} r0={180} r1={120} d0={i * 30} d1={((i+1) * 30)}/>
-                })
-            }
-            {
-                ...CIRCLE_OF_FIFTHS_DATA.map((v, i) => {
-                    return <CircleOfFifthsWedge onClick={() => {handleClick(v.relativeMinor)}} r0={120} r1={80} d0={i * 30} d1={((i+1) * 30)}/>
-                })
-            }
-            {
-                ...CIRCLE_OF_FIFTHS_DATA.map((v, i) => {
-                    return <CircleOfFifthsWedge r0={80} r1={50} d0={i * 30} d1={((i+1) * 30)} />
-                })
-            }
-            </g>
-            <g>
-            {
-                ...CIRCLE_OF_FIFTHS_DATA.map((v, i) => {
-                    const [center_x, center_y] = polarToCartesian(200, 200, 150, (i *30))
-                    return <text style={{textAnchor: "middle", dominantBaseline: "central", pointerEvents: "none"}} x={center_x} y={center_y} >{v.note}</text>
-                })
-            }
-            {
-                ...CIRCLE_OF_FIFTHS_DATA.map((v, i) => {
-                    const [center_x, center_y] = polarToCartesian(200, 200, 100, (i *30))
-                    return <text style={{textAnchor: "middle", dominantBaseline: "central", fontSize: "small", pointerEvents: "none"}} x={center_x} y={center_y} >{v.relativeMinor}</text>
-                })
-            }
-            {
-                ...CIRCLE_OF_FIFTHS_DATA.map((v, i) => {
-                    const [center_x, center_y] = polarToCartesian(200, 200, 65, (i *30))
-                    return <text style={{textAnchor: "middle", dominantBaseline: "central", fontSize: "xx-small", pointerEvents: "none"}} x={center_x} y={center_y} >{v.diminished}</text>
-                })
-            }
-            </g>
-            </svg>
+return <svg version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox={`0 0 ${CF_SIZE} ${CF_SIZE}`} >
+
+        <g style={{transform: "rotate(-15deg)", transformOrigin: `${CF_CENTER}px ${CF_CENTER}px`}}>
+        {
+            ...CIRCLE_OF_FIFTHS_DATA.map((v, i) => {
+                return <CircleOfFifthsWedge onClick={() => {handleClick(v.note)}} r0={180} r1={120} d0={i * 30} d1={((i+1) * 30)} selectable={true}/>
+            })
+        }
+        {
+            ...CIRCLE_OF_FIFTHS_DATA.map((v, i) => {
+                return <CircleOfFifthsWedge onClick={() => {handleClick(v.relativeMinor)}} r0={120} r1={80} d0={i * 30} d1={((i+1) * 30)} selectable={true}/>
+            })
+        }
+        {
+            ...CIRCLE_OF_FIFTHS_DATA.map((v, i) => {
+                return <CircleOfFifthsWedge r0={80} r1={50} d0={i * 30} d1={((i+1) * 30)} />
+            })
+        }
+        </g>
+        <g>
+        {
+            ...CIRCLE_OF_FIFTHS_DATA.map((v, i) => {
+                const [center_x, center_y] = polarToCartesian(200, 200, 150, (i *30))
+                return <text style={{textAnchor: "middle", dominantBaseline: "central", pointerEvents: "none"}} x={center_x} y={center_y} >{v.note}</text>
+            })
+        }
+         {
+            ...CIRCLE_OF_FIFTHS_DATA.map((v, i) => {
+                const [center_x, center_y] = polarToCartesian(200, 200, 100, (i *30))
+                return <text style={{textAnchor: "middle", dominantBaseline: "central", fontSize: "small", pointerEvents: "none"}} x={center_x} y={center_y} >{v.relativeMinor}</text>
+            })
+        }
+        {
+            ...CIRCLE_OF_FIFTHS_DATA.map((v, i) => {
+                const [center_x, center_y] = polarToCartesian(200, 200, 65, (i *30))
+                return <text style={{textAnchor: "middle", dominantBaseline: "central", fontSize: "xx-small", pointerEvents: "none"}} x={center_x} y={center_y} >{v.diminished}</text>
+            })
+        }
+        </g>
+        </svg>
 
 }
 
 type CircleOfFifthsWedgeProps = {
+    /**
+     * The outer radius of the segment, i.e. how far out it extends from the center of the circle.
+     */
     r0: number;
+    /**
+     * The inner radius of the segment, i.e. where it starts. If this were 0, the segment would touch the center.
+     */
     r1: number;
+    /**
+     * The starting position of the segment in the circle in degrees from the reference point.
+     */
     d0: number;
+    /**
+     * The ending position of the segment in the circle in degrees from the reference point.
+     */
     d1: number;
-    fillColor?: string;
-    hoverColor?: string;
-    selectedColor?: string;
-    strokeColor?: string;
+    /**
+     * The callback function to be called when the CircleOfFifthsWedge is clicked.
+     */
     onClick?: () => void;
+    selectable?: boolean;
 }
 
 const CircleOfFifthsWedge = (props: CircleOfFifthsWedgeProps) => {
-    /**
-     * @param r0 - outer radius of the segment, i.e. how far out it extends from the center of the circle
-     * @param r1 - inner radius of the segment - i.e. where it starts. if this were 0, the segment would touch the center
-     * @param d0 - where the segment starts in the circle - i.e. the degrees from reference of one edge
-     * @param d1 - where the segment end in the circle - i.e. the degrees from reference of other edge
-     * @param onClick - onClick
-     */
     return (
         <path
             d={segmentPath(200, 200, props.r0, props.r1, props.d0, props.d1)}
+            className={`cf-wedge ${props.selectable? "selectable" : ""}`}
             stroke="black"
             strokeWidth="2"
             fill="#84CC16"
-            onClick={props.onClick} ></path>
+            onClick={props.onClick? props.onClick : undefined} ></path>
     )
 }
---
+++ src/demo-app/App.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { useState } from "react";
+
+import { CircleOfFifths } from "../CircleOfFifths";
+
+export function App() {
+  const [key, setKey] = useState("C");
+
+  return <>
+    <h1 style={{textAlign: "center"}}>Key Explorer</h1>
+
+    <div style={{margin: "auto", height: "30%", width: "30%"}}>
+    <CircleOfFifths handleKeySelection={setKey}/>
+    </div>
+
+    <h1 style={{textAlign: "center"}}>{key}</h1>
+  </>
+
+}
